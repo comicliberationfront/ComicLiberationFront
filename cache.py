e4966181e094c523bcbf69675fbb8639e7a8a731
Comic Liberation Front <comicliberationfront@gmail.com>
2012-07-26 16:14:25 -0700

    Added support for file caching.

--- cache.py
+++ cache.py
@@ -1,5 +1,7 @@
 import os
 import os.path
+import json
+import time
 import datetime
 import logging
 
@@ -18,18 +20,26 @@ class Cache(object):
         self.logger = logger
 
     def has(self, key):
-        return key in self.items
+        if key in self.items:
+            return True
+        path = self._get_item_path(key)
+        if path and os.path.isfile(path):
+            return True
+        return False
 
     def get(self, key, raise_on_miss=False):
-        if not key in self.items:
+        item = self._get_item(key)
+        if item is None:
             if raise_on_miss:
                 raise Exception("Cache miss, key not found: %s" % key)
             self.logger.debug('Cache miss (reason: not found): %s' % key)
             return None
-        item = self.items[key]
-        if item['end_time'] < datetime.datetime.now():
+        if item['end_time'] < time.time():
             self.logger.debug('Cache miss (reason: outdated): %s' % key)
             self.items.pop(key)
+            path = self._get_item_path(key)
+            if path and os.path.isfile(path):
+                os.delete(path)
             return None
         self.logger.debug('Cache hit: %s' % key)
         return item['data']
@@ -37,9 +47,32 @@ class Cache(object):
     def set(self, key, data, lifetime=None):
         if not lifetime:
             lifetime = self.default_lifetime
-        self.items[key] = {
-                'end_time': datetime.datetime.now() + lifetime,
+        item = {
+                'end_time': time.time() + lifetime.total_seconds(),
                 'data': data
                 }
+        self.items[key] = item
+        path = self._get_item_path(key)
+        if path:
+            with open(path, 'w') as f:
+                json.dump(item, f)
         self.logger.debug('Cached: %s' % key)
+
+    def _get_item_path(self, key):
+        if not self.cache_dir:
+            return None
+        return os.path.join(self.cache_dir, '%s.json' % key)
+
+    def _get_item(self, key):
+        if not key in self.items:
+            path = self._get_item_path(key)
+            if path and os.path.isfile(path):
+                self.logger.debug('Loading from disk cache: ' % key)
+                with open(path, 'r') as f:
+                    item = json.load(f)
+                    self.items[key] = item
+                    return item
+        if key in self.items:
+            return self.items[key]
+        return None
 
